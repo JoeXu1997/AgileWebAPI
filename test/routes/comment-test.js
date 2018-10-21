2b3318e35cc1a560b0304cc1fec607c4eae026c5
xu yue <joexu1997@gmail.com>
2018-10-21 14:10:24 +0100

    Get all comments test

--- test/routes/comment-test.js
+++ test/routes/comment-test.js
@@ -6,6 +6,24 @@ let expect = chai.expect;
 chai.use(chaiHttp);
 let _ = require('lodash' );
 
-describe('Donations', function (){
-    // TODO
+describe('Comments', function (){
+    describe('GET /comment',  () => {
+        it('should return all the donations in an array', function(done) {
+            chai.request(server)
+                .get('/comment')
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.a('array');
+                    expect(res.body.length).to.equal(18);
+                    // let result = _.map(res.body, (donation) => {
+                    //     return { id: donation.id,
+                    //         amount: donation.amount }
+                    // });
+                    // expect(result).to.include( { id: 1000000, amount: 1600  } );
+                    // expect(result).to.include( { id: 1000001, amount: 1100  } );
+                    // done();
+                    done();
+                });
+        });
+    });
 });
