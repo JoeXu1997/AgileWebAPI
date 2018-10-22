ff1b9a1d7323295cdc5aeee0a341362486d1a8fb
xu yue <joexu1997@gmail.com>
2018-10-22 17:52:55 +0100

    add Hash code for password

--- models/users.js
+++ models/users.js
@@ -1,7 +1,9 @@
 let mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
 // db.userdb.insert({
 //     username: "admin",
 //     password: "admin",
+//     usertype:"admin",
 //     actions: {
 //         upvotefor: "Inception",
 //         comment:{
@@ -13,8 +15,9 @@ let mongoose = require('mongoose');
 let UserSchema = new mongoose.Schema({
         username: String,
         password: String,
+        usertype: String,
         actions:{
-            upvotefor:String,
+            upvotefor:{type:String,default:""},
             comment:{
                 commentfor:[],         //default: new Array()
                 content:[]
@@ -22,4 +25,14 @@ let UserSchema = new mongoose.Schema({
         }
     },
     { collection: 'userdb' });
+UserSchema.pre('save', function (next) {
+    var user = this;
+    bcrypt.hash(user.password, 10, function (err, hash){
+        if (err) {
+            return next(err);
+        }
+        user.password = hash;
+        next();
+    })
+});
 module.exports = mongoose.model('User', UserSchema);
