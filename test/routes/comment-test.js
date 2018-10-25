b8c9c649ab71ff98a1dc1a88ad26d30f86b2914b
xu yue <joexu1997@gmail.com>
2018-10-25 17:50:48 +0100

    add comment test

--- test/routes/comment-test.js
+++ test/routes/comment-test.js
@@ -7,19 +7,46 @@ chai.use(chaiHttp);
 let _ = require('lodash' );
 
 describe('Comments', function (){
-    describe('GET /comment',  () => {
-        it('should return all the donations in an array', function(done) {
+    // describe('GET /comment',  () => {
+    //     it('should return all the comments in an array', function(done) {
+    //         chai.request(server)
+    //             .get('/comment')
+    //             .end((err, res) => {
+    //                 expect(res).to.have.status(200);
+    //                 expect(res.body).to.be.a('array');
+    //                 expect(res.body.length).to.equal(1);
+    //                 done();
+    //             });
+    //     });
+    // });
+    describe('POST /donations', function () {
+        it('should return confirmation message and update database(add a new comment)', function(done) {
+            let comment = {
+                username: 'xu' ,
+                commentfor: "Dangal",
+                content: "nice movie"
+            };
             chai.request(server)
-                .get('/comment')
-                .end((err, res) => {
+                .post('/comment')
+                .send(comment)
+                .end(function(err, res) {
                     expect(res).to.have.status(200);
-                    expect(res.body).to.be.a('array');
-                    expect(res.body.length).to.equal(1);
+                    expect(res.body).to.have.property('message').equal('Comment Add Successful!' );
                     done();
                 });
         });
     });
-    describe('PUT /comment',()=>{
-       it('')
-    });
+    // describe('PUT /comment/:id', () => {
+    //     it('should return a comment and the comment content should be different with before', function(done) {
+    //         chai.request(server)
+    //             .put('/comment/5bd1c916f6bf492830ff24ec')
+    //             .send({commentfor:"Inception",content:"new nice flim"})
+    //             .end(function(err, res) {
+    //                 expect(res).to.have.status(200);
+    //                 let donation = res.body.data ;
+    //                 expect(donation).to.include( { _id: "5bd1c916f6bf492830ff24ec", content: "new nice flim"  } );
+    //                 done();
+    //             });
+    //     });
+    // });
 });
