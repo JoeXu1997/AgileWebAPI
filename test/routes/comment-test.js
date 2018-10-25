6dd2bc9dbc413b7697c257f5160d3abdbfc1443b
xu yue <joexu1997@gmail.com>
2018-10-25 18:14:14 +0100

    delete comment test

--- test/routes/comment-test.js
+++ test/routes/comment-test.js
@@ -14,28 +14,28 @@ describe('Comments', function (){
     //             .end((err, res) => {
     //                 expect(res).to.have.status(200);
     //                 expect(res.body).to.be.a('array');
-    //                 expect(res.body.length).to.equal(1);
+    //                 expect(res.body.length).to.equal(1);//according to collection comment
     //                 done();
     //             });
     //     });
     // });
-    describe('POST /donations', function () {
-        it('should return confirmation message and update database(add a new comment)', function(done) {
-            let comment = {
-                username: 'xu' ,
-                commentfor: "Dangal",
-                content: "nice movie"
-            };
-            chai.request(server)
-                .post('/comment')
-                .send(comment)
-                .end(function(err, res) {
-                    expect(res).to.have.status(200);
-                    expect(res.body).to.have.property('message').equal('Comment Add Successful!' );
-                    done();
-                });
-        });
-    });
+    // describe('POST /donations', function () {
+    //     it('should return confirmation message and update database(add a new comment)', function(done) {
+    //         let comment = {
+    //             username: 'xu' ,
+    //             commentfor: "Dangal",
+    //             content: "nice movie"
+    //         };
+    //         chai.request(server)
+    //             .post('/comment')
+    //             .send(comment)
+    //             .end(function(err, res) {
+    //                 expect(res).to.have.status(200);
+    //                 expect(res.body).to.have.property('message').equal('Comment Add Successful!' );
+    //                 done();
+    //             });
+    //     });
+    // });
     // describe('PUT /comment/:id', () => {
     //     it('should return a comment and the comment content should be different with before', function(done) {
     //         chai.request(server)
@@ -44,9 +44,23 @@ describe('Comments', function (){
     //             .end(function(err, res) {
     //                 expect(res).to.have.status(200);
     //                 let donation = res.body.data ;
-    //                 expect(donation).to.include( { _id: "5bd1c916f6bf492830ff24ec", content: "new nice flim"  } );
+    //                 expect(donation).to.include( { _id: "5bd1c916f6bf492830ff24ec", content: "new nice flim"  } );//depends on existing comments
     //                 done();
     //             });
     //     });
     // });
+    describe('DELETE /comment/:id',() => {
+        it('should return a message and delete a donation record', function (done) {
+            chai.request(server)
+                .delete('/comment/5bd1f7c2329ef129b6b54d0a')
+                .end(function (err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('message', 'Delete Successful');
+                    let comment = res.body.data;
+                    expect(comment.commentfor).is.to.equal("Roman Holiday");
+                    expect(comment.content).is.to.equal("Nice film");
+                    done();
+                });
+        });
+    });
 });
