b36040a53d1b196eba8519c991053f59ef6ccb6e
xu yue <joexu1997@gmail.com>
2018-11-03 18:51:16 +0000

    correct some mistakes

--- test/routes/movie-test.js
+++ test/routes/movie-test.js
@@ -13,38 +13,35 @@ describe('Movie API', function (){
     beforeEach(function(done){
         var newMovie = new Movie({
             name:"A Chinese Odyssey",
-            movietype: 'Comedy',
+            movietype: "Comedy",
             Directedby:"ZhenWei Liu",
             mainActor:"XingChi Zhou",
             upvotes: 1
         });
-        var newMovie1 =  new Movie({
-            name:"Roman Holiday",
-            movietype: 'Romance',
-            Directedby:"William Wyler",
-            mainActor:"Audrey Hepburn"
-        });
-        newMovie1.save(function(err) {
-            done();
-            newMovie.save();
+        newMovie.save(function(err,data) {
+            id = data._id
+            done();console.log("add done")
         });
     });
     afterEach(function(done){
+        Movie.collection.drop();console.log("delete")
         done();
-        Movie.collection.drop();
     });
     describe("GET functions",function () {
-        describe('GET /movies',  function(){
-            before(function (done) {
-                var newMovie2 = new Movie({
-                    name:"Mission:Impossible",
-                    movietype: 'Suspense',
-                    Directedby:"Brian Russell De Palma",
-                    mainActor:"TomCruise"
-                });
-                newMovie2.save(done);
-            })
-            it('should return all the movies in an array ordered by upvotes', function(done) {
+        beforeEach(function (done) {
+            var movie = new Movie({
+                name: "made",
+                movietype: 'Comedy',
+                Directedby: "Zhen me Liu",
+                mainActor: "Xing me",
+                upvotes: 2
+            });
+            movie.save(function (err) {
+                done();
+            });
+        });
+        describe('GET /movies',  function() {
+            it('should return all the movies in an array ordered by upvotes', function (done) {
                 chai.request(server)
                     .get('/movies')
                     .end((err, res) => {
@@ -55,131 +52,120 @@ describe('Movie API', function (){
                     });
             });
         });
-        describe('GET /movies/actor/:mainActor',function () {
-            it('should return movies which has someone as actor ', function(done) {
-                chai.request(server)
-                    .get('/movies/actor/Hepburn')
-                    .end((err, res) => {
-                        expect(res).to.have.status(200);
-                        expect(res.body.length).to.equal(1);
-                        expect(res.body[0]).to.have.property("name").equal("Roman Holiday");
-                      //  expect(res.body[0]).to.have.property("name").equal("made");
-                        done();
-                    });
-            });
-            it('should return empty array if there is no matching result', function (done) {
-                chai.request(server)
-                    .get('/movies/actor/Audaarey')
-                    .end((err, res) => {
-                        expect(res).to.have.status(200);
-                        expect(res.body.length).to.equal(0);
-                        done();
-                    });
-            });
+        describe('GET /movies/actor/:mainActor', function () {
+                it('should return movies which has someone as actor ', function (done) {
+                    chai.request(server)
+                        .get('/movies/actor/Xing')
+                        .end((err, res) => {
+                            expect(res).to.have.status(200);
+                            expect(res.body.length).to.equal(2);
+                            expect(res.body[0]).to.have.property("name").equal("A Chinese Odyssey");
+                            expect(res.body[1]).to.have.property("name").equal("made");
+                            done();
+                        });
+                });
+                it('should return empty array if there is no matching result', function (done) {
+                    chai.request(server)
+                        .get('/movies/actor/Audaarey')
+                        .end((err, res) => {
+                            expect(res).to.have.status(200);
+                            expect(res.body.length).to.equal(0);
+                            done();
+                        });
+                });
         });
-        describe('GET /movies/director/:Directedby',function () {
-            it('should return movies which has someone as director', function(done) {
-                chai.request(server)
-                    .get('/movies/director/Liu')
-                    .end((err, res) => {
-                        expect(res).to.have.status(200);
-                        expect(res.body.length).to.equal(1);
-                       // expect(res.body[1]).to.have.property("name").equal("made");
-                        expect(res.body[0]).to.have.property("name").equal("A Chinese Odyssey");
-                        done();
-                    });
-            });
-            it('should return empty array if there is no matching result', function (done) {
-                chai.request(server)
-                    .get('/movies/director/Liuaaaa')
-                    .end((err, res) => {
-                        expect(res).to.have.status(200);
-                        expect(res.body.length).to.equal(0);
-                        done();
-                    });
-            });
+        describe('GET /movies/director/:Directedby', function () {
+                it('should return movies which has someone as director', function (done) {
+                    chai.request(server)
+                        .get('/movies/director/Zhen')
+                        .end((err, res) => {
+                            expect(res).to.have.status(200);
+                            expect(res.body.length).to.equal(2);
+                            expect(res.body[1]).to.have.property("name").equal("made");
+                            expect(res.body[0]).to.have.property("name").equal("A Chinese Odyssey");
+                            done();
+                        });
+                });
+                it('should return empty array if there is no matching result', function (done) {
+                    chai.request(server)
+                        .get('/movies/director/Liuaaaa')
+                        .end((err, res) => {
+                            expect(res).to.have.status(200);
+                            expect(res.body.length).to.equal(0);
+                            done();
+                        });
+                });
         });
-        describe('GEt /movies/:movietype',function () {
-            it('should return movies according to types ', function(done) {
-                chai.request(server)
-                    .get('/movies/Comedy')
-                    .end((err, res) => {
-                        expect(res).to.have.status(200);
-                        expect(res.body.length).to.equal(1);
-                        expect(res.body[0]).to.have.property("name").equal("A Chinese Odyssey");
-                        done();
-                    });
-            });
-            it('should return empty array if no matching results ', function(done) {
-                chai.request(server)
-                    .get('/usr/upvote/asd')
-                    .end((err, res) => {
-                        expect(res).to.have.status(200);
-                        expect(res.body).to.be.a('array');
-                        expect(res.body.length).to.equal(0);
-                        done();
-                    });
-            });
+        describe('GET /movies/:movietype',function () {
+                it('should return movies according to types ', function (done) {
+                      chai.request(server)
+                       .get('/movies/Comedy')
+                       .end((err, res) => {
+                         expect(res).to.have.status(200);
+                         expect(res.body.length).to.equal(2);
+                         expect(res.body[0]).to.have.property("name").equal("A Chinese Odyssey");
+                         expect(res.body[1]).to.have.property("name").equal("made");
+                         done();
+                     });
+                 });
+                it('should return empty array if no matching results ', function (done) {
+                     chai.request(server)
+                      .get('/usr/upvote/asd')
+                      .end((err, res) => {
+                            expect(res).to.have.status(200);
+                            expect(res.body).to.be.a('array');
+                           expect(res.body.length).to.equal(0);
+                           done();
+                     });
+                 });
         });
     });
     describe('POST functions',function () {
-        describe('POST /movies', function () {
-            it('should return success message and update database(add a new movie)', function(done) {
-                let movie = {
-                    "name": "test",
-                    "movietype": "Horror",
-                    "Directedby": "me",
-                    "mainActor":"me"
-                }
-                chai.request(server)
-                    .post('/addmoviestest')
-                    .send(movie)
-                    .end(function(err, res) {
-                        expect(res).to.have.status(200);
+        it('should return success message and update database(add a new movie)', function(done) {
+            let movie = {
+                "name": "test",
+                "movietype": "Horror",
+                "Directedby": "me",
+                "mainActor":"me"
+            }
+            chai.request(server)
+                .post('/addmoviestest')
+                .send(movie)
+                .end(function(err, res) {
+                    expect(res).to.have.status(200);
                         expect(res.body).to.have.property('message').equal('Movie Add Successful!');
                         done();
-                    });
-            });
-            afterEach(function  (done) {
-                chai.request(server)
-                    .get('/movies')
-                    .end(function(err, res) {
-                        let result = _.map(res.body, (movie) => {
-                            return { name: movie.name,
-                                movietype: movie.movietype,
-                                Directedby:movie.Directedby,
-                                mainActor:movie.mainActor};
-                        }  );
-                        expect(result[2]).to.include( {
-                            name: "test",
-                            movietype: "Horror",
-                            Directedby: "me",
-                            mainActor:"me"  } );
-                        done();
-                    });
-            });  // end-after
-        });
-    });
-    describe('PUT functions',function () {
-        describe('PUT /movies/:id', () => {
-            it('should return success message and add 1 to movie upvotes', function(done) {
-                var newUser1 = new User({
-                    username: 'yue',
-                    password: '123456',
-                    usertype: 'admin',
-                    actions:{
-                        upvotefor:"",
-                        comment:{
-                            commentfor:[],
-                            content:[]
-                        }
-                    }
                 });
-                newUser1.save();
-                Movie.findOne({"name":"A Chinese Odyssey"},function (err,movie) {
-                    id=movie._id;
+        });
+        afterEach(function  (done) {
+            console.log("in afer")
+            chai.request(server)
+                .get('/movies')
+                .end(function(err, res) {
+                    let result = _.map(res.body, (movie) => {
+                        return { name: movie.name,
+                            movietype: movie.movietype,
+                            Directedby:movie.Directedby,
+                            mainActor:movie.mainActor};
+                    }  );
+                    expect(result[1]).to.have.property("name").equal("test");
                     done();
                 });
+        });  // end-after
+    });
+    describe('PUT functions',function () {
+        afterEach(function (done) {
+            User.collection.drop();
+            done();
+        })
+        it('should return success message and add 1 to movie upvotes', function(done) {
+            var newUser1 = new User({
+                username: 'yue',
+                password: '123456',
+                usertype: 'admin',
+            });
+            newUser1.save();
+            Movie.find({},function (err,movie) {
                 chai.request(server)
                     .put('/movies/'+id)
                     .send({"operator":"yue"})
@@ -190,25 +176,23 @@ describe('Movie API', function (){
                         expect(movie).to.have.property('upvotes').equal(2);
                         done();
                     });
-            });
-            it('should failed message if the operator already vote for other movies', function(done) {
-                var newUser = new User({
-                    username: 'xu',
-                    password: '123456',
-                    usertype: 'admin',
-                    actions:{
-                        upvotefor:"Inception",
-                        comment:{
-                            commentfor:["Inception"],
-                            content:["Good Film"]
-                        }
+            })
+        });
+        it('should failed message if the operator already vote for other movies', function(done) {
+            var newUser = new User({
+                username: 'xu',
+                password: '123456',
+                usertype: 'admin',
+                actions:{
+                    upvotefor:"Inception",
+                    comment:{
+                        commentfor:["Inception"],
+                        content:["Good Film"]
                     }
-                });
-                newUser.save();
-                Movie.findOne({"name":"A Chinese Odyssey"},function (err,movie) {
-                    id=movie._id;
-                    done();
-                });
+                }
+            });
+            newUser.save();
+            Movie.find({},function (err,movie) {
                 chai.request(server)
                     .put('/movies/'+id)
                     .send({"operator":"xu"})
@@ -216,46 +200,46 @@ describe('Movie API', function (){
                         expect(res.body).to.have.property('message').equal("Each user could only vote for one movie");
                         done();
                     });
-            });
+            })
         });
     });
-    describe('DELETE /movies/:id',() => {
+    describe('DELETE /movies/:id',function() {
+        afterEach(function (done) {
+            User.collection.drop();
+            done();
+        })
         it('should return success message and delete one movie', function(done) {
             var newUser1 = new User({
                 username: 'test',
                 password: '123456',
                 usertype: "admin",
             });
-            newUser1.save();
-            Movie.find({},function (err,movie) {
+            newUser1.save(function (err,data) {
                 chai.request(server)
-                    .delete('/movies/'+movie[0]._id)
-                    .send({"operator":"test"})
+                    .delete('/movies/'+id)
+                    .send({"operator":data.username})
                     .end(function(err, res) {
                         expect(res).to.have.status(200);
                         expect(res.body).to.have.property('message').equal("Delete Successful");
                         done();
                     });
-            })
-
-        });
+            });
+        })
         it('should failed message if the operator not an admin', function(done) {
             var newUser = new User({
                 username: 'test2',
                 password: '123456',
                 usertype: "common",
             });
-            newUser.save();
-            Movie.find({},function (err,movie){
+            newUser.save(function (err,data) {
                 chai.request(server)
-                    .delete('/movies/'+movie[0]._id)
-                    .send({"operator":"test2"})
+                    .delete('/movies/'+id)
+                    .send({"operator":data.username})
                     .end(function(err, res) {
                         expect(res.body).to.have.property('message').equal("You donot have this authority");
                         done();
                     });
-            })
-
-        });
+            });
+        })
     });
 });
